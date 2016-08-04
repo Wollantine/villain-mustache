141d15ed60c5ebfb034eabc0e70a660de61593cc
Marc Rodriguez <marc.r@enabl.es>
2016-08-04 18:01:44 +0200

    Added boilerplate. Added test specs

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const villainMustache = (label, context) => {
+    return label;
+}
+
+export default villainMustache;
---
+++ test/index.js
@@ -0,0 +1,32 @@
+import chai from 'chai';
+chai.should();
+
+describe('Villain Mustache', () => {
+    it('Should not alter labels without mustache markup', () => {
+
+    });
+
+    it('Should replace {{var}} with var\'s value', () => {
+
+    });
+
+    it('Should compile {{#if}} to the adequate contents', () => {
+
+    });
+
+    it('Should compile nested {{#if}}s correctly', () => {
+
+    });
+
+    it('Should keep unmatched {{#elsif}}, {{#else}}, {{#endif}}', () => {
+
+    });
+
+    it('Should throw when there is no {{#endif}}', () => {
+
+    });
+
+    it('Should escape \\{', () => {
+
+    });
+});
